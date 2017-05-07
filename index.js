56776227f86ceb847525ec6d8c76f3abe2a41a35
Martin McAllister <martin@iammartinmcallister.com>
2017-05-07 23:19:11 +0100

    Twit working stream

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var TwitterPackage = require('twitter');
+var TwitterPackage = require('twit');
 
 // importing my secret.json file
 var secret = require("./secret");
@@ -10,43 +10,20 @@ var config = require("./config");
 console.log(config);
 
 
-
-
-
 //make a new Twitter object
 var Twitter = new TwitterPackage(secret);
 
 // Call the stream function and pass in 'statuses/filter', our filter object, and our callback
-Twitter.stream('statuses/filter', {track: config['search-queries'], filter: 'verified'}, function(stream) {
-//Twitter.stream('statuses/filter', {track: 'twitter'}, function(stream) {
+//Twitter.stream('statuses/filter', {track: config['search-queries'], filter: 'verified'}, function(stream) {
+//var stream = Twitter.stream('statuses/filter', {track: 'twitter'});
+var stream = Twitter.stream('statuses/filter', {track: config['search-queries'], filter: 'verified'});
 
 
-  // ... when we get tweet data...
-  stream.on('data', function(tweet) {
+stream.on('tweet', function (tweet) {
+  console.log(tweet)
+})
 
-    // print out the text of the tweet that came in
-    console.log(tweet.text);
-
-    //build our reply object
-    //var statusObj = {status: "Hi @" + tweet.user.screen_name + ", How are you?"}
-
-    //call the post function to tweet something
-    /*Twitter.post('statuses/update', statusObj,  function(error, tweetReply, response){
-
-      //if we get an error print it out
-      if(error){
-        console.log(error);
-      }
-
-      //print the text of the tweet we sent out
-      //console.log(tweetReply.text);
-    });*/
-  });
-
-  // ... when we get an error...
-  stream.on('error', function(error) {
-    //print out the error
-    console.log(error);
-    
-  });
+// ... when we get an error...
+stream.on('error', function(error) {
+    console.log(error);    
 });
